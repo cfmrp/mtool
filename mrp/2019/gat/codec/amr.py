2b544330f9b7b68daeb869b0f6859397187b4c1a
Stephan Oepen <oe@ifi.uio.no>
2019-03-22 19:28:25 +0000

    change of heart regarding separate nodes for INSTANCE values (aka concepts)
    git-svn-id: file:///home/oe/lib/mrp/tmp/svn@608 3df82f5b-d43a-0410-af33-fce91db48ec5

--- mrp/2019/gat/codec/amr.py
+++ mrp/2019/gat/codec/amr.py
@@ -19,27 +19,6 @@ def amr_lines(fp):
             else:
                 lines.append(line)
 
-def amr2graph_old(id, amr):
-    assert len(amr.nodes) == len(set(amr.nodes))
-    graph = Graph(id)
-    node2id = {}
-    for i, node in enumerate(amr.nodes):
-        node2id[node] = i
-        graph.add_node()
-    instance_triples, attribute_triples, relation_triples = smatch.get_triples()
-    for type, arg1, arg2 in relation_triples:
-        assert arg1 in node2id and arg2 in node2id
-        src = node2id[arg1]
-        tgt = node2id[arg2]
-        edge = graph.add_edge(src, tgt, type)
-    for type, arg1, _ in attribute_triples:
-        if type == "TOP":
-            graph.nodes[node2id[arg1]].is_top = True
-    for type, arg1, arg2 in instance_triples:
-        assert type == "instance"
-        
-    return graph
-
 def amr2graph(id, amr):
     graph = Graph(id)
     node2id = {}
@@ -47,11 +26,18 @@ def amr2graph(id, amr):
     for n, v, a in zip(amr.nodes, amr.node_values, amr.attributes):
         id = i
         node2id[n] = id
-        graph.add_node(id, top=("TOP" in a))
+        graph.add_node(id, label = v, top=("TOP" in a))
         i += 1
-        graph.add_node(i, label=v)
-        graph.add_edge(id, i, "instance")
-        i += 1
+        #
+        # creating separate 'atom' nodes for the concepts and
+        # 'instance' edges is how SMATCH scores (inspired by
+        # EDM_n, i suspect), but really these extra elements of
+        # structure seem superfluous, seeing as we have the
+        # notion of a designated label property on nodes.
+        #
+#        graph.add_node(i, label=v)
+#        graph.add_edge(id, i, "instance")
+#        i += 1
         for key, val in a.items():
             if key != "TOP":
                 graph.add_node(i, label=val)
