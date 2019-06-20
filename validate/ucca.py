e8d30e6420f8860fd513199bf7e8e399105a77b9
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-06-20 17:04:12 +0200

    Fix typo

--- validate/ucca.py
+++ validate/ucca.py
@@ -6,7 +6,7 @@ from validate.utilities import report
 def test(graph, actions, stream=sys.stderr):
     n = 0
     for edge in graph.edges:
-        if not isinstance(edge.label, str) or len(edge.label) == 0:
+        if not isinstance(edge.lab, str) or len(edge.lab) == 0:
             n += 1
             report(graph,
                    "missing or invalid label",
