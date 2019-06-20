5a49e9ccb83742d1bf1eea5d879a3491ea480397
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-06-20 17:02:56 +0200

    #6 Check for non-empty edge label in UCCA validation

--- validate/ucca.py
+++ validate/ucca.py
@@ -1,9 +1,14 @@
-import sys;
+import sys
 
-from graph import Graph;
-from validate.utilities import report;
+from validate.utilities import report
 
-def test(graph, actions, stream = sys.stderr):
-  n = 0;
-  return n;
 
+def test(graph, actions, stream=sys.stderr):
+    n = 0
+    for edge in graph.edges:
+        if not isinstance(edge.label, str) or len(edge.label) == 0:
+            n += 1
+            report(graph,
+                   "missing or invalid label",
+                   edge=edge, stream=stream)
+    return n
