bc80890e6ccde06ec6e5820bc45f7ceb24552e7c
Stephan Oepen <oe@ifi.uio.no>
2019-06-24 22:13:12 +0200

    a little more high-level validation (for a first public announcement)

--- validate/core.py
+++ validate/core.py
@@ -8,10 +8,36 @@ from validate.utilities import report;
 
 def test(graph, actions, stream = sys.stderr):
   n = 0;
+  if not isinstance(graph.id, str) or len(graph.id) == 0:
+    n += 1;
+    report(graph,
+           "missing or invalid ‘id’ property",
+           stream = stream);
+  if not isinstance(graph.flavor, int) or graph.flavor not in {0, 1, 2}:
+    n += 1;
+    report(graph,
+           "missing or invalid ‘flavor’ property",
+           stream = stream);
+  if not isinstance(graph.framework, str) or \
+     graph.framework not in {"ccd", "dm", "pas", "psd", "ud",
+                             "eds", "ucca", "amr"}:
+    n += 1;
+    report(graph,
+           "missing or invalid ‘framework’ property",
+           stream = stream);
+  elif graph.flavor == 0 and \
+       graph.framework not in {"ccd", "dm", "pas", "psd", "ud"} or \
+       graph.flavor == 1 and graph.framework not in {"eds", "ucca"} or \
+       graph.flavor == 2 and graph.framework not in {"amr"}:
+    n += 1;
+    report(graph,
+           "invalid Flavor ({}) framework: {}"
+           "".format(graph.flavor, graph.framework), stream = stream);
+
   if "input" in actions:
     if not isinstance(graph.input, str) or len(graph.input) == 0:
       n += 1;
-      report(graph, 
+      report(graph,
              "missing or invalid ‘input’ property",
              stream = stream);
 
@@ -31,7 +57,7 @@ def test(graph, actions, stream = sys.stderr):
                stream = stream);
       if edge.tgt not in nodes:
         n += 1;
-        report(graph, 
+        report(graph,
                "invalid target",
                node = node, edge = edge,
                stream = stream);
