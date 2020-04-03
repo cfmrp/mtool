e4b045d9dbabff964b9b93e2b7048f44707b0cb7
Stephan Oepen <oe@ifi.uio.no>
2020-04-04 01:52:08 +0200

    first steps towards a Treex (input) codec

--- codec/treex.py
+++ codec/treex.py
@@ -21,33 +21,35 @@ def walk(id, node, parent, nodes, edges, ns):
     for child in children:
       if child.tag == ns + "LM":
         walk(id, child, i, nodes, edges, ns);
+    if children.find(ns + "LM") is None:
+      walk(id, children, i, nodes, edges, ns);
 
 def read(fp, text = None):
   ns = "{http://ufal.mff.cuni.cz/pdt/pml/}";
   tree = ET.parse(fp).getroot();
   bundles = tree.find(ns + "bundles");
-  item = bundles.find(ns + "LM");
-  if item is not None:
-      id = item.get("id");
-      graph = Graph(id, flavor = 0, framework = "ptt");
-      for zone in item.iter(ns + "zone"):
-        if zone.get("language") == "en":
-          sentence = zone.findtext(ns + "sentence");
-          trees = zone.find(ns + "trees");
-          if trees is not None:
-            atree = trees.find(ns + "a_tree");
-            ttree = trees.find(ns + "t_tree");
-            print(id, sentence, ttree);
-            root = ttree.find(ns + "children");
-            if root is not None:
-              nodes = list(); edges = list();
-              walk(id, root, None, nodes, edges, ns);
-      mapping = {};
-      for node in sorted(nodes, key = itemgetter(1)):
-        mapping[node[0]] = i = len(mapping);
-        lemma = node[2].findtext(ns + "t_lemma");
-        graph.add_node(id = i, label = lemma, top = node[2].tag == ns + "children");
+  for item in bundles.findall(ns + "LM"):
+    id = item.get("id");
+    graph = Graph(id, flavor = 0, framework = "ptt");
+    for zone in item.iter(ns + "zone"):
+      if zone.get("language") == "en":
+        sentence = zone.findtext(ns + "sentence");
+        trees = zone.find(ns + "trees");
+        if trees is not None:
+          atree = trees.find(ns + "a_tree");
+          ttree = trees.find(ns + "t_tree");
+          print(id, sentence, ttree);
+          root = ttree.find(ns + "children");
+          top = root.get("id");
+          if root is not None:
+            nodes = list(); edges = list();
+            walk(id, root, None, nodes, edges, ns);
+    mapping = {};
+    for node in sorted(nodes, key = itemgetter(1)):
+      mapping[node[0]] = i = len(mapping);
+      lemma = node[2].findtext(ns + "t_lemma");
+      graph.add_node(id = i, label = lemma, top = node[0] == top);
 #      print(len(nodes), nodes, edges);        
-      for source, target, label in edges:
-        graph.add_edge(mapping[source], mapping[target], label);
-      yield graph, None;
+    for source, target, label in edges:
+      graph.add_edge(mapping[source], mapping[target], label);
+    yield graph, None;
