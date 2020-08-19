877e608ed3a7e2b78e40a6ccf886ebcb59391b1c
Stephan Oepen <oe@ifi.uio.no>
2020-08-20 00:23:58 +0200

    more diagnostic output

--- inspector.py
+++ inspector.py
@@ -16,16 +16,19 @@ def summarize(graphs, golds):
         ids[language][target].add(gold.id);
 
   counts = dict();
+  seen = dict();
   targets = dict();
   targets["eng"] = ["eds", "ptg", "ucca", "amr", "drg"];
   targets["ces"] = ["ptg"];
   targets["deu"] = ["ucca", "drg"];
   targets["zho"] = ["amr"];
   for language in ["eng", "ces", "deu", "zho"]:
-    if language not in counts: counts[language] = dict();
+    counts[language] = dict();
+    seen[language] = dict();
     for key in targets[language]:
-      if key not in counts[language]: counts[language][key] = 0;
-  
+      counts[language][key] = 0;
+      seen[language][key] = set();
+
   for graph in graphs:
     language = graph.language();
     if language is None: language = "eng";
@@ -34,6 +37,12 @@ def summarize(graphs, golds):
        language in ids and framework in ids[language] and \
        graph.id in ids[language][framework]:
       counts[language][framework] += 1;
+      if graph.id in seen[language][framework]:
+        print("inspector.summarize(): ignoring duplicate {} {} graph #{}."
+              "".format(language, framework, graph.id),
+              file = sys.stderr);
+      else:
+        seen[language][framework].add(graph.id);
 
   complete = True;
   for language in ["eng", "ces", "deu", "zho"]:
